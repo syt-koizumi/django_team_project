deb908b31d57d01f0fea63939d73e74f427bbfd8
syt-koizumi <syt-koizumi@usen-next.jp>
2024-06-11 13:53:36 +0900

    confのurlsで各アプリのルーティングを作成

--- conf/urls.py
+++ conf/urls.py
@@ -5,4 +5,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("toppage.urls")),
+    path("accounts",include("accounts.urls")),
+    path("mypage",include("mypage.urls")),
+    path("movie",include("movie.urls")),
+    path("usersearch",include("usersearch.urls")),
 ]
