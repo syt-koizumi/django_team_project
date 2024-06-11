bd8db55b3714a96209dd0c931a61ee1ff8e6dcf0
syt-koizumi <syt-koizumi@usen-next.jp>
2024-06-11 17:55:05 +0900

    movieに検索機能の途中まで追加しました。

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from toppage.views import TopPageView
+from .views import MovieSearchView,MovieSearchResultView
 app_name = "movie"
 urlpatterns = [
-      path('', TopPageView.as_view(),name = "toppage"),
+      path('/search', MovieSearchView.as_view(),name = "movie_search"),
+      path('/search/result/<str:movie_name>/<str:resultdata>',  MovieSearchResultView.as_view(),name = "search_result"),
+      
 ]
