9f9c780977e6e4576c921c278c89ba594247c8d2
syt-koizumi <syt-koizumi@usen-next.jp>
2024-06-11 18:51:38 +0900

    検索画面で文字を入力すると検索結果がjson形式で返却する機能の追加

--- movie/urls.py
+++ movie/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 from .views import MovieSearchView,MovieSearchResultView
 app_name = "movie"
 urlpatterns = [
-      path('/search', MovieSearchView.as_view(),name = "movie_search"),
-      path('/search/result/<str:movie_name>/<str:resultdata>',  MovieSearchResultView.as_view(),name = "search_result"),
+      path('search', MovieSearchView.as_view(),name = "movie_search"),
+      path('search/result/<str:movie_name>',  MovieSearchResultView.as_view(),name = "search_result"),
       
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,7 @@
 
 
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 from django.views.generic import FormView
 from django.shortcuts import render
 from .forms import MovieSearchForm
@@ -10,10 +10,12 @@ from .Api import  TMDB
 from django.urls import reverse_lazy
 from django.views.generic.edit import ModelFormMixin
 #検索結果の一覧を表示するTemplateView
-class MovieSearchResultView(TemplateView,ModelFormMixin):
-    template_name = "movie/movie_search_result.html"
-   
 
+class MovieSearchResultView(ListView):
+    template_name = 'movie/movie_search_result.html'
+    
+
+   
 
 
 #検索画面の作成を行う
@@ -25,16 +27,11 @@ class MovieSearchView(FormView):
     def form_valid(self, form):
         # フォームのデータを取得して、次のビューに渡す
         movie_name = form.cleaned_data['movie_name']
-        resultdata = GetMovieApi(movie_name)
-        
-        return redirect('movie:search_result', movie_name=movie_name, resultdata=resultdata)
-        # 成功後のURLにデータを渡す
-        self.success_url = reverse_lazy('movie:search_result', kwargs={'movie_name': movie_name, 'resultdata': resultdata})
-        return super().form_valid(form)
-    
+        data = GetMovieApi(movie_name)
+        return render(self.request, 'movie/movie_search_result.html',{"movie_name":data})
     
 def GetMovieApi(movie_name):
     api = TMDB() # tokenは発行された文字列を代入
     res = api.search_movies(movie_name)
-    res['results']
-    return "aaa"
+    return str(res['results'])
+    
