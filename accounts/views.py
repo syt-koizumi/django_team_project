11643600d5c02c703a45eb6b9e6f44c6173a2522
saigusa-kiichi <kc-saigusa@usen-next.jp>
2024-06-11 17:50:56 +0900

    make-account
    Co-authored-by: km-imai <km-imai@users.noreply.github.com>
    Co-authored-by: taizotz <taizotz@users.noreply.github.com>

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,43 @@
+from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import MyUserCreationForm, CustomUserCreationForm,LoginAuthenticationForm
+from .models import CustomUser
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.base import TemplateView
+
+
+class AccountCreateView(generic.CreateView):
+    Model = User
+    form_class = UserCreationForm
+    template_name = 'accounts/accounts_create.html'
+    success_url = "/accounts/accounts_create"
+
+
+class AccountCreateViewUsingMyForm(generic.CreateView):
+    Model = User
+    form_class = MyUserCreationForm
+    template_name = 'accounts/accounts_create.html'
+    success_url = "/accounts/accounts_create_with_form"
+
+
+class CustomAccountCreationView(generic.CreateView):
+    Model = CustomUser
+    form_class = CustomUserCreationForm
+    template_name = 'accounts/accounts_create.html'
+    success_url = '/accounts/custom_accounts_create'
+
+class Login(LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = LoginAuthenticationForm
+    
+
+
+class Logout(LogoutView):
+    template_name = 'accounts/logout.html'
+    #next_page = '/accounts/login.html'
+
+
+class Home(TemplateView):
+    template_name = 'accounts/home.html'
 
