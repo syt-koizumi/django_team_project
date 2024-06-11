805f952098fe7f0b11510d2f3c07d21230cbe1b2
syt-koizumi <syt-koizumi@usen-next.jp>
2024-06-11 12:28:12 +0900

    必要なアプリの作成、setting.pyでアプリの追加

--- conf/settings.py
+++ conf/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "mypage.apps.MypageConfig",
+    "toppage.apps.ToppageConfig",
+    "usersearch.apps.UsersearchConfig",
+    "accounts.apps.AccountsConfig",
+    "movie.apps.MovieConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
