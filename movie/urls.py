fda404fa4ce32b721733dc4c694f828d114f9e84
syt-koizumi <syt-koizumi@usen-next.jp>
2024-06-11 14:20:09 +0900

    urlsの設定の変更

--- movie/urls.py
+++ movie/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from toppage.views import TopPageView
 app_name = "movie"
 urlpatterns = [
       path('', TopPageView.as_view(),name = "toppage"),
