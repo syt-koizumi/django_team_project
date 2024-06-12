35c1b2e912a390cb738b6ca75e7e816453e6e451
saigusa-kiichi <kc-saigusa@usen-next.jp>
2024-06-12 15:35:00 +0900

    make-mypage
    Co-authored-by: taizotz <taizotz@users.noreply.github.com>

--- mypage/views.py
+++ mypage/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-    
+from movie.models import MyMovieModel
+from django.contrib.auth.mixins import LoginRequiredMixin
 
     
-class MylistView(TemplateView):
+    
+class MylistView(LoginRequiredMixin, TemplateView):
     template_name = 'mypage/mylist.html'
+
+    def get(self, request):
+
+        MyMovieModel_data = MyMovieModel.objects.filter(createUser=self.request.user)
+        context = {
+            'MyMovieModel_data': MyMovieModel_data
+        }
+        return render(request, 'mypage/mylist.html', context)
+
+    # def get(request, id):
+
+    #     MyMovieModel_data = get_object_or_404(MyMovieModel, pk = id)
+    #     context = {
+    #         'MyMovieModel_data': MyMovieModel_data
+    #     }
+        
+    #     return render(request, 'mypage/mylist.html', context)
