ab3f3db55e917f6cb5d8abd19f0c9da8bd3556b5
syt-koizumi <syt-koizumi@usen-next.jp>
2024-06-14 14:39:33 +0900

    おすすめ機能の作成途中

--- consider/views.py
+++ consider/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView,ListView,CreateView, FormView,View
+from django.urls import reverse_lazy
+from django.views.generic.edit import ModelFormMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, get_object_or_404
 
-# Create your views here.
+class ConsiderView(TemplateView):
+    template_name = "consider/consider.html"
+    
