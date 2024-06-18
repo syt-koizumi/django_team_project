52e987dd3d9a698376a0513567d1f1567736906e
tsubasa423 <tsubasa283928@gmail.com>
2024-06-18 13:53:42 +0900

    変更

--- mail/views.py
+++ mail/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import send_user
+from django.urls import reverse_lazy
 
 class create_mail(ListView):
-    template_name = 'mail/create_mail.html'
     model = send_user
-
+    template_name = 'mail/create_mail.html'
+    context_object_name = 'title'
+    ordering = ['-dtcreated']  #日付で逆順に並べ替える
