a932146bbd27246dcce299acd28c6ef9e149bb0e
syt-koizumi <syt-koizumi@usen-next.jp>
2024-06-19 13:28:40 +0900

    topic機能を作成するための準備を行いました。

--- conf/settings.py
+++ conf/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "topic.apps.TopicConfig",
     "mail.apps.MailConfig",
     "comment.apps.CommentConfig",
     "ranking.apps.RankingConfig",
