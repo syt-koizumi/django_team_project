52598ac729815427c927a1d9a634715f35e77d29
saigusa-kiichi <kc-saigusa@usen-next.jp>
2024-06-19 17:59:21 +0900

    add mail Kidoku

--- mail/models.py
+++ mail/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from accounts.models import CustomUser
 
-class send_user(models.Model):
+class Mail(models.Model):
     title = models.CharField(max_length=30)
     text = models.TextField(max_length=100)
     dtcreated = models.DateTimeField(auto_now_add=True)
@@ -8,3 +9,10 @@ class send_user(models.Model):
     def __str__(self):
         return self.title
     
+class ReadMail(models.Model):
+    read_mail = models.ForeignKey(Mail, verbose_name="メール", related_name='read_mail', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, verbose_name="ユーザ名", related_name='user', on_delete=models.CASCADE)
+    read_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.read_mail)
--- mail/views.py
+++ mail/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import send_user
+from django.shortcuts import render,redirect
+from django.views.generic import ListView, DetailView, View
+from .models import Mail,ReadMail
 from django.urls import reverse_lazy
+# from .forms import CheckMailForm
 
-class create_mail(ListView):
-    model = send_user
-    template_name = 'mail/create_mail.html'
-    context_object_name = 'title'
-    ordering = ['-dtcreated']  #日付で逆順に並べ替える
+class Mail_list(View):
+    def get(self, request):
+        mails = Mail.objects.all().order_by('-dtcreated')
+
+        return render(request, 'mail/mail_box.html', {
+            'mails': mails
+        })
+
+
+class Mail_detail(DetailView):
+    model = Mail
+    template_name = 'mail/detail_mail.html'
+    
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        mail = self.get_object()
+        read_mail, created = ReadMail.objects.get_or_create(read_mail=mail,user=request.user)
+        if not created:
+            read_mail.read_count += 1
+            read_mail.save()
+        # ReadMail.objects.create(read_mail=self.get_object(), user=request.user)
+        return response
