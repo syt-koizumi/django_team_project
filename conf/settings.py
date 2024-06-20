b281c0ae9abc7d8ad501fd1fec2ae170477ef751
syt-koizumi <syt-koizumi@usen-next.jp>
2024-06-20 16:26:40 +0900

    settings.pyを追加

---
+++ conf/settings.py
@@ -0,0 +1,136 @@
+import os
+import environ
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()  # 追加
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+
+INSTALLED_APPS = [
+    "topic.apps.TopicConfig",
+    "mail.apps.MailConfig",
+    "comment.apps.CommentConfig",
+    "ranking.apps.RankingConfig",
+    "mypage.apps.MypageConfig",
+    "toppage.apps.ToppageConfig",
+    "usersearch.apps.UsersearchConfig",
+    "consider.apps.ConsiderConfig",
+    "accounts.apps.AccountsConfig",
+    "movie.apps.MovieConfig",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'conf.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+
+WSGI_APPLICATION = 'conf.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+#本番環境では、こっちのコメントアウトを消して上をコメントアウトする
+#DATABASES = {
+#      'default': {
+#          'ENGINE': env('ENGINE'),
+#          'NAME': env('NAME'),
+#          'USER': 'movie',
+#          'PASSWORD': env('PASSWORD'),
+#          'HOST': env('HOST'),
+#          'PORT': env('PORT'),
+#      }
+#  }
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'ja'
+
+TIME_ZONE = 'Asia/Tokyo'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
