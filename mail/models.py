1d9a95de6b058ea5e3dbc4a99568557809747a35
saigusa-kiichi <kc-saigusa@usen-next.jp>
2024-06-20 12:13:31 +0900

    mail機能css以外実装完了
    Co-authored-by: km-imai <km-imai@users.noreply.github.com>
    Co-authored-by: taizotz <taizotz@users.noreply.github.com>
    Co-authored-by: tsubasa423 <tsubasa423@users.noreply.github.com>

--- mail/models.py
+++ mail/models.py
@@ -5,6 +5,7 @@ class Mail(models.Model):
     title = models.CharField(max_length=30)
     text = models.TextField(max_length=100)
     dtcreated = models.DateTimeField(auto_now_add=True)
+    # user = 
 
     def __str__(self):
         return self.title
--- mail/urls.py
+++ mail/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Mail_list, Mail_detail
+from .views import Mail_list, Mail_detail, Unread_list
 
 app_name = 'mail'
 urlpatterns = [
     path('', Mail_list.as_view(), name='mail_list'),
+    path('unread/', Unread_list.as_view(), name='unread_mail_list'),
     path('detail/<int:pk>', Mail_detail.as_view(), name='mail_detail'),
 ]
