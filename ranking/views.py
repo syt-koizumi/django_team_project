16d8ef827d9e88df8ad1c62ea4431a57fd2b5da6
km-imai <km-imai@unext-hd.jp>
2024-06-21 14:25:53 +0900

    better ranking

--- ranking/views.py
+++ ranking/views.py
@@ -22,6 +22,23 @@ class MovieRankingView(ListView):
         #MyMovieModelからデータを取得してAllMovie変数に格納
         # AllMovie = MyMovieModel.objects.all()
         
-        RankingMovie = MyMovieModel.objects.values('imagepath', 'name', 'overview').annotate(movie_count=Count('createUser')).order_by('-movie_count')[0:10]
+        RankingMovie = MyMovieModel.objects.values('imagepath', 'name', 'overview').annotate(movie_count=Count('createUser')).order_by('-movie_count')
+        
+        now_rank = 1
+        kaburi_count = 0
+        last_score = None
+        result_rank = []
 
-        return RankingMovie
+        for movie in RankingMovie:
+            score = movie['movie_count']
+            if last_score is not None and score != last_score:
+                now_rank += kaburi_count
+                kaburi_count = 0
+            last_score = score
+            movie['rank'] = now_rank
+            result_rank.append(movie)
+            kaburi_count += 1
+
+        return result_rank[:10]
+
+        # return render(self.request,'ranking/ranking.html',context)
