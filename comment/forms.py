ca275bd20f8414266373f38d7d465eb4af7d03f2
saigusa-kiichi <kc-saigusa@usen-next.jp>
2024-06-28 11:49:59 +0900

    フィルターの被りなし修正

--- comment/forms.py
+++ comment/forms.py
@@ -14,9 +14,16 @@ class CommentForm(forms.ModelForm):
         if user:
             self.fields['movie_name'].queryset = MyMovieModel.objects.filter(createUser=user)
 
+# class MovieFilterForm(forms.Form):
+#     movie_name = forms.ModelChoiceField(
+#         queryset=MyMovieModel.objects.filter(movie_comments__isnull=False).distinct("name"),
+#         required=False,
+#         label='Filter by Movie'
+#     )
+
 class MovieFilterForm(forms.Form):
-    movie_name = forms.ModelChoiceField(
-        queryset=MyMovieModel.objects.filter(movie_comments__isnull=False).distinct(),
+    movie_name = forms.ChoiceField(
+        choices=[('', '---------')] + [(movie['name'], movie['name']) for movie in MyMovieModel.objects.filter(movie_comments__isnull=False).values('name').distinct()],
         required=False,
         label='Filter by Movie'
     )
